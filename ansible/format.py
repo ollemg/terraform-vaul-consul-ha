d4d1a0b68fd2b0215a0a0523d67d52b2177b9efb
Gabriel Barbosa <gabrielmello808@gmail.com>
2022-02-25 00:26:36 -0300

    hosts dinâmicos no ansible

--- ansible/format.py
+++ ansible/format.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-from hosts import *
 import yaml
+from hosts import *
 from yaml.loader import SafeLoader
 
 ansible_vars = f"""
@@ -14,19 +14,20 @@ with open('hosts.ini', 'w') as ansible:
     ansible.write('[all]\n')
     ansible.close()
 
-for ips in vms:
+for ip_index in ip_address:
     with open('hosts.ini', 'a') as ansible:
-        ansible.write(f'{ips}\n')
+        ansible.write(f'{ip_index}\n')
         ansible.close()
 
 with open('hosts.ini', 'a') as ansible:
     ansible.write(ansible_vars)
     ansible.close()
 
-ansible_vars = dict(zip(names, vms))
 
+ansible_vars = dict(zip(hostname, ip_address))
+ansible_vars['total_servers'] = total_servers
 
-with open('hostname.yml', 'w') as yaml_file:
+with open('terraform.yml', 'w') as yaml_file:
     yaml.dump(ansible_vars, yaml_file, default_flow_style=False)
 
 
